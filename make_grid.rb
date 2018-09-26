21c2a11f1a36b74bfd86e39d96062f5869df6ee3
bonochof <ryos427@yahoo.co.jp>
2018-09-26 09:30:53 +0900

    refactor

--- make_grid.rb
+++ make_grid.rb
@@ -1,10 +1,4 @@
 def make_grid (string)
-  grid = 9.times.collect{ Array.new(9, nil) }
-  0.upto(8) do |i|
-    0.upto(8) do |j|
-      grid[i][j] = (string[i*9+j] == "." ? nil : string[i*9+j].to_i)
-    end
-  end
-  grid
+  (0..8).collect{|i| string[i*9, 9].split(//).collect{|c| c == "." ? nil : c.to_i}}
 end
 

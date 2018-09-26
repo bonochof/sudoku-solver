28a055989df8c982d1f160efe2311d3352c329a3
bonochof <ryos427@yahoo.co.jp>
2018-09-26 09:33:07 +0900

    to var

--- print_grid.rb
+++ print_grid.rb
@@ -1,4 +1,4 @@
-def print_grid (grid)
-  print grid.collect{|line| line.collect{|v| (v || '.')}.join('')}.join("\n"), "\n"
+def print_grid (grid, pad=" ")
+  print grid.collect{|line| line.collect{|v| (v || '.')}.join('')}.join(pad), "\n"
 end
 

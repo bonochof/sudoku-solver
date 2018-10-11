1819c95ba9b909f0a6eaa97a11fcfc983e1579a1
bonochof <ryos427@yahoo.co.jp>
2018-10-11 10:42:42 +0900

    impl cell class

--- basic_rule/sudoku.rb
+++ basic_rule/sudoku.rb
@@ -1,5 +1,25 @@
 module Sudoku
   class Cell
+    attr_reader :id, :val, :possible
+    
+    def initialize (id, val)
+      @id = id
+      @val = val
+      @possible = (val ? [] : (1..9).to_a)
+      @block = []
+    end
+    
+    def external_form ()
+      "C-#{id/9+1}-#{id%9+1}"
+    end
+    
+    def set_block (block)
+      case block.type
+      when :row then @block[0] = block
+      when :column then @block[1] = block
+      when :square then @block[2] = block
+      end
+    end
   end
   
   class Block
